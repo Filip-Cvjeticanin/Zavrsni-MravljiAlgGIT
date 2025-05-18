9680cec400d015018543ca5896dd66008e47ca12
Filip-Cvjeticanin <filip.cvjeticanin@fer.unizg.hr>
2025-05-18 19:24:43 +0200

    Cleanup

---
+++ headers/outputControll.h
@@ -0,0 +1,8 @@
+//
+// Created by Filip on 5/18/2025.
+//
+
+#ifndef OUTPUTCONTROLL_H
+#define OUTPUTCONTROLL_H
+
+#endif //OUTPUTCONTROLL_H
---
+++ sources/outputControll.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Filip on 5/18/2025.
+//
+
+#include "../headers/outputControll.h"
